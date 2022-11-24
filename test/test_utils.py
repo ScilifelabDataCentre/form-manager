2fdfc3582a47cd2dba1a86aea2a04cc5a421cf6c
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-11-24 12:53:48 +0100

    Add test for gen_json_body

--- test/test_utils.py
+++ test/test_utils.py
@@ -24,7 +24,26 @@ def test_apply_template():
     }
 
     res = utils.apply_template(BASE_TEMPLATE, data)
+    print(res)
     assert res.count("hit_1") == 2
     assert res.count("hit_2") == 1
     assert "BAD" not in data
     assert "unique }}" not in data
+
+
+def test_gen_json_body():
+    """Test gen_json_body()."""
+    data = {
+        "val": "hit_1",
+        "asd": "hit_2",
+        "variable": "hit_3",
+        "spec_val": "unique",
+        "bad_variable": "BAD",
+    }
+
+    expected = ('{\n  "asd": "hit_2",\n  "bad_variable": "BAD",\n  ',
+                '"spec_val": "unique",\n  "val": "hit_1",\n  "variable": "hit_3"\n}')
+
+    res = utils.gen_json_body(data)
+    assert res.startswith(expected)
+    assert "Submission received:" in res
