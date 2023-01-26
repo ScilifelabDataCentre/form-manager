697d24e7db0c1771b00423c93d6e3650de2baf3c
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2023-01-26 13:19:14 +0100

    Add blacklist tests

--- test/test_forms.py
+++ test/test_forms.py
@@ -80,3 +80,39 @@ def test_add_list_delete_form_not_allowed(client):
     helpers.login(helpers.USERS[0], client)
     response = client.delete(f"/api/v1/form/{identifier}")
     assert response.status_code == 200
+
+
+def test_receive_submission_blacklist_accept(client, form_default):
+    """
+    1. Submit a form
+    2. Verify that it is accepted by the blacklist
+    """
+    data = {"blKey": "abc"}
+    response = client.post(f"/api/v1/form/{form_default}/incoming", data=data)
+    assert response.status_code == 302
+    assert response.location == "/success?redirect=https://www.example.com"
+    assert helpers.data_source.fetch_submissions(form_default)
+
+
+def test_receive_submission_blacklist_reject(client, form_default):
+    """
+    1. Submit a form
+    2. Verify that it is rejected by the blacklist
+    """
+    data = {"blKey": "123"}
+    response = client.post(f"/api/v1/form/{form_default}/incoming", data=data)
+    assert response.status_code == 302
+    assert response.location == "/success?redirect=https://www.example.com"
+    assert not helpers.data_source.fetch_submissions(form_default)
+
+
+def test_receive_submission_blacklist_bad_regex_accept(client, form_bad_bl):
+    """
+    1. Submit a form
+    2. Verify that it is accepted by the blacklist
+    """
+    data = {"blKey": "123"}
+    response = client.post(f"/api/v1/form/{form_bad_bl}/incoming", data=data)
+    assert response.status_code == 302
+    assert response.location == "/success?redirect=https://www.example.com"
+    assert helpers.data_source.fetch_submissions(form_bad_bl)
