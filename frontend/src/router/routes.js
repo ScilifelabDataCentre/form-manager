992cffaa7eb9717dd7abbaf4cdc6306dea0d4fcd
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2025-04-04 16:57:24 +0200

    Migrate to `pnpm` end update dependencies

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -30,8 +30,7 @@ const routes = [
   },
 
   
-  // Always leave this as last one,
-  // but you can also remove it
+  // Always leave this as last one, but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
